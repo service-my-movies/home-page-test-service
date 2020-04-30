60631571d445bcd4ed46bee8c7cdbc264f3d35ef
Ranushan Rachu <ranusha.n@hotmail.fr>
2020-04-30 16:30:31 +0200

    [ORIGIN][MASTER][UPDATED] centralize config, add dockerfile and add crossorigine

--- src/main/java/com/mymovies/controller/HomeController.java
+++ src/main/java/com/mymovies/controller/HomeController.java
@@ -1,12 +1,16 @@
 package com.mymovies.controller;
 
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RefreshScope
 public class HomeController {
 
     @RequestMapping("/")
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     public String getHomePage() {
         return "index.html";
     }
